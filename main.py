e46367ce248d312c869cff701b85f73996348aa0
Gramding <skyfilexd@Googlemail.com>
2025-02-13 16:22:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-mport flet as ft
-
+import flet as ft
 
 def main(page: ft.Page):
     page.title = "Flet counter example"
-    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
@@ -19,13 +17,12 @@ def main(page: ft.Page):
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
                 txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
     )
-
 
 ft.app(main, view=ft.AppView.WEB_BROWSER)
